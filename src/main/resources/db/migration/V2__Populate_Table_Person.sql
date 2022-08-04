fbf876571e806a0f11b9d4a05dd9899b1f52f05d
felipesekai <felippe10_@hotmail.com>
2022-08-04 11:49:55 -0300

    migrations

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `person` VALUES (12346,'rua tal taltlads','sekai','male','felipe',NULL),
+                            (12350,'brasilian address number 10','jose','male','joaquin',NULL),
+                            (12358,'brasilian address number 10','mario','male','joaquin',NULL),
+                            (12360,'brasilian address number 1510','jorge','male','jose','12/12/12');
+
