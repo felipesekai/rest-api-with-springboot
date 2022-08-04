7ef82af5ff9ee3badc178642c6d12e65cf5e9407
felipesekai <felippe10_@hotmail.com>
2022-08-04 11:59:08 -0300

    custom Json

--- src/main/kotlin/br/com/sekai/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/sekai/data/vo/v1/PersonVO.kt
@@ -1,10 +1,17 @@
 package br.com.sekai.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 
+@JsonPropertyOrder("id","name", "lastName" ,"gender"  ,"address")
 data class PersonVO(
     var id: Long = 0,
+    @JsonProperty("name")
     var firstName: String = "",
     var lastName: String = "",
     var address: String = "",
+
+    @JsonIgnore //nao envia esse campo
     var gender: String = "",
 )
